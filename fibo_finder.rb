2022d6da3aa92ff6c6062043cbed2e2fb9427f91
apradhn <abhijit.pradhan6@gmail.com>
2015-08-28 14:29:45 -0400

    completes challenge

--- fibo_finder.rb
+++ fibo_finder.rb
@@ -1,3 +1,16 @@
 def fibo_finder(n)
-  # code goes here
+  if n == 0 || n == 1
+    n
+  else
+    a = 1
+    b = 1
+    fib = a + b
+    i = 2
+    while i < n
+      fib = a + b
+      b, a = a, fib
+      i += 1
+    end
+    fib
+  end
 end
